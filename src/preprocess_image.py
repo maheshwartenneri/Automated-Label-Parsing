9bd48b29113b8271ce66e554c6dd5dd336edc1ff
bma2015 <bmabbe@gmail.com>
2022-09-18 22:51:57 -0400

    Cleaning up code, adding comments for remove shadow

--- src/preprocess_image.py
+++ src/preprocess_image.py
@@ -8,13 +8,14 @@ if __name__ == "__main__":
     # construct the argument parser and parse the arguments
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", type=str, help="path to input image")
+    ap.add_argument("-t", "--transf", type=str, help="transformations to apply: r(otation), c(rop), ")
     args = vars(ap.parse_args())
     
     image_path = args["image"]
     base_file_name = os.path.basename(image_path)
     
     img = cv2.imread(image_path)
-    result_no_norm, result_with_norm = remove_shadows(img)
+    result_norm = remove_shadows(img)
     
     cv2.imwrite('../images/preprocessed_' + base_file_name, result_with_norm)
     
--- src/remove_shadows.py
+++ src/remove_shadows.py
@@ -2,8 +2,7 @@ import cv2
 import numpy as np
 
 '''
-Takes a cv2 image and returns two images: one with shadows removed without normalization and one with
-shadows removed with normalization
+Takes a cv2 image and returns two images: one with shadows removed with normalization
 '''
 def remove_shadows(img):
     rgb_planes = cv2.split(img)
@@ -11,14 +10,23 @@ def remove_shadows(img):
     result_planes = []
     result_norm_planes = []
     for plane in rgb_planes:
+        # Dilate the image, in order to get rid of the text. This step somewhat helps to preserve a bar code.
         dilated_img = cv2.dilate(plane, np.ones((7,7), np.uint8))
+        
+        # Median blur the result with a decent sized kernel to further suppress any text.
+        # This should result in an acceptable background image that contains all the shadows and/or discoloration.
         bg_img = cv2.medianBlur(dilated_img, 21)
+        
+        # Calculate the difference between the original and the background image just obtained. The bits that are
+        # identical will be black (close to 0 difference), the text will be white (large difference).
+        # Since black on white is desired, the result is inverted.
         diff_img = 255 - cv2.absdiff(plane, bg_img)
+        
+        # Normalize the image, so that the full dynamic range is used.
         norm_img = cv2.normalize(diff_img,None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
-        result_planes.append(diff_img)
+        
         result_norm_planes.append(norm_img)
 
-    result = cv2.merge(result_planes)
     result_norm = cv2.merge(result_norm_planes)
     
-    return result, result_norm
+    return result_norm
